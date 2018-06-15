b713383b09690158291b4cb90dff0f0a1b34b6af
Raphael Huefner <raphaelhuefner@gmail.com>
2018-06-16 00:16:22 +0200

    factor out headless run to be re-used between test-running and seeding of expected values

---
+++ test_browserify/gather-tokenizations.js
@@ -0,0 +1,35 @@
+const puppeteer = require('puppeteer');
+const express = require('express');
+const staticServe = require('express-static');
+
+module.exports = function gatherTokenizations() {
+  return new Promise((gatherResolve, gatherReject) => {
+    const server = express()
+      .use(staticServe(__dirname + '/public/'))
+      .listen(3000, async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+
+        const outputPromise = new Promise((outputResolve, outputReject) => {
+          page.on('console', async msg => {
+            if (
+              (2 == msg.args().length)
+              &&
+              ('tokenization' == await msg.args()[0].jsonValue())
+            ) {
+              let tokenizationJSON = await msg.args()[1].jsonValue();
+              outputResolve(JSON.parse(tokenizationJSON));
+            }
+          });
+        });
+
+        await page.goto('http://localhost:3000/index.html', {waitUntil: 'networkidle0'});
+
+        let tokenization = await outputPromise;
+
+        await browser.close();
+        server.close();
+        gatherResolve(tokenization);
+      });
+  });
+}
--- test_browserify/overwrite-expected-tokenizations.js
+++ test_browserify/overwrite-expected-tokenizations.js
@@ -1,45 +1,13 @@
-const puppeteer = require('puppeteer');
-const express = require('express');
-const staticServe = require('express-static');
+const gatherTokenizations = require('./gather-tokenizations.js');
 const writeFilePromise = require('fs-writefile-promise');
 
 const DATADIR = __dirname + '/data';
 
-const gatherPromise = new Promise((gatherResolve, gatherReject) => {
-  const server = express()
-    .use(staticServe(__dirname + '/public/'))
-    .listen(3000, async () => {
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-
-      const outputPromise = new Promise((outputResolve, outputReject) => {
-        page.on('console', async msg => {
-          if (
-            (2 == msg.args().length)
-            &&
-            ('tokenization' == await msg.args()[0].jsonValue())
-          ) {
-            let tokenizationJSON = await msg.args()[1].jsonValue();
-            outputResolve(JSON.parse(tokenizationJSON));
-          }
-        });
-      });
-
-      await page.goto('http://localhost:3000/index.html', {waitUntil: 'networkidle0'});
-
-      let tokenization = await outputPromise;
-
-      await browser.close();
-      server.close();
-      gatherResolve(tokenization);
-    });
-});
-
 async function overwrite() {
-  const tokenization = await gatherPromise;
-  Object.keys(tokenization).forEach(async function (key) {
+  const tokenizations = await gatherTokenizations();
+  Object.keys(tokenizations).forEach(async function (key) {
     let jsonFileName = `${DATADIR}/${key}.json`;
-    let jsonFileContents = JSON.stringify(tokenization[key], null, 2);
+    let jsonFileContents = JSON.stringify(tokenizations[key], null, 2);
     jsonFileContents += "\n"; // to satisfy `git diff`
     await writeFilePromise(jsonFileName, jsonFileContents);
   });
